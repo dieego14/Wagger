a47c2933b286d4f636e711d8f9d096603a02d23a
Diego <dieegohernandez14@gmail.com>
2023-05-01 18:33:56 -0700

    Made Home Page Form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,24 +11,59 @@ const Home = () => {
           </h1>
           <div className="bg-gray-100 w-[320px] h-[450px] rounded-lg py-8 px-6">
             {/* Form */}
-            <form className=" mb-0">
-              {/* Label */}
-              <label for="email" className="block text-black font-medium">
-                Name
-              </label>
-              {/* Input */}
-              <div className="mt-1">
-              <input type="text" id="fname" name="fname" required
-               className="w-full border border-gray-300 text-black px-3 py-2 rounded-lg
-               shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1
-               focus:ring-indigo-500"/>
+            <form className="mb-0 space-y-3">
+              {/* First Name */}
+              <div>
+                {/* Label */}
+                <label for="fname" className="block text-black font-medium">
+                  Name
+                </label>
+                {/* Input */}
+                <div className="mt-1">
+                  <input
+                    type="text" id="fname" name="fname" required
+                    className="form-input"
+                  />
+                </div>
               </div>
+              <div>
+                {/* Last Name */}
+                <label for="lname" className="block text-black font-medium">Last Name</label>
+                <div className="mt-1">
+                  <input 
+                    type="text" id="lname" name="lname" required 
+                    className="form-input" />
+                </div>
+              </div>
+              {/* Email */}
+              <div>
+                <label for="email" className="block text-black font-medium">Email</label>
+                <div className="mt-1">
+                  <input 
+                    type="email" id="email" name="email" required 
+                    className="form-input" />
+                </div>
+              </div>
+              {/* Address */}
+              <div>
+                <label for="address" className="block text-black font-medium">Address</label>
+                <div className="mt-1">
+                  <input 
+                    type="email" id="email" name="email" required 
+                    className="form-input" />
+                </div>
+              </div>
+              {/* Submit */}
+              <input type="submit" value="Book!"
+               className="w-full py-2 my-10 bg-yellow-300 rounded-full text-black
+               hover:bg-yellow-500 active:bg-yellow-600"/>
             </form>
           </div>
         </div>
         <img
-        className="w-full max-h-[500px] object-cover"
-        src="https://images.unsplash.com/photo-1494947665470-20322015e3a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
+          className="w-full max-h-[500px] object-cover"
+          src="https://images.unsplash.com/photo-1494947665470-20322015e3a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        />
       </div>
     </div>
   );
